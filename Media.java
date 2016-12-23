0de6947057b7708e342b17eb7de3e888c99436c6
Illuminix <a.oakes4@students.clark.edu>
2016-12-22 17:01:44 -0800

    Update Media.java

--- Media.java
+++ Media.java
@@ -9,44 +9,16 @@ import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
 public class Media extends Scrape {
-    public Image getImage(URL url) {
-        Image image = null;
-        try {
-            image = ImageIO.read(url);
-        } catch (IOException e) {
-        }
-        return image;//Returns image bytes
-    }
-    public InputStream mp3In(URL url) {
-        URLConnection connect = null;
-        try {
-            connect = url.openConnection();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        InputStream in_stream = null;
-        try {
-            in_stream = connect.getInputStream();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return in_stream;
-    }
-    public void mp3Save(InputStream stream, File file) throws IOException {
-        OutputStream outstream = null;
-        try {
-            outstream = new FileOutputStream(file);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+   public static void mp3Save(String url,String path) throws IOException {
+        URLConnection conn = new URL(url).openConnection();
+        InputStream is = conn.getInputStream();
+
+        OutputStream outstream = new FileOutputStream(new File(path));
         byte[] buffer = new byte[4096];
         int len;
-        while ((len = stream.read(buffer)) > 0) {
-            try {
-                outstream.write(buffer, 0, len);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        while ((len = is.read(buffer)) > 0) {
+            outstream.write(buffer, 0, len);
         }
+        outstream.close();
     }
 }
